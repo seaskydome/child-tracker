7cf0cc8c0aa8f8449f36265ae953faebc46e166f
seaskydome <seaskydome@gmail.com>
2023-01-24 09:33:31 -0600

    Setup frontend with react and bootstrap

---
+++ frontend/src/App.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Button } from 'react-bootstrap';
+
+function App() {
+
+  const [clickCount, setClickCount] = useState(0);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Yay
+        </p>
+        {/* Bootstrap button! */}
+        <Button onClick={() => setClickCount(clickCount + 1)}>
+          Clicked {clickCount} times
+        </Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
