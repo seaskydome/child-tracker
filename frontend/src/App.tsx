cafc8cef0b8a7869aff85b9ea3304b12767c551d
seaskydome <seaskydome@gmail.com>
2023-01-24 09:55:17 -0600

    FETCHED THE NOTES FROM THE SERVER! and proxy

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,38 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Button } from 'react-bootstrap';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "react-bootstrap";
+import { Child } from "./models/child";
 
 function App() {
+  const [children, setChildren] = useState<Child[]>([]);
 
-  const [clickCount, setClickCount] = useState(0);
+  useEffect(() => {
+    async function loadNotes() {
+      try {
+        // basic js way to fetch, the second argument is the type of command
+        // we added the PROXY in the package.json which is where we fetch from
+        // and CORS wont have a problem
+        const response = await fetch("/api/children", {
+        method: "GET",
+        });
+
+        // parse the json cuz remember we are sending json
+        const children = await response.json();
+        setChildren(children);
+
+      } catch (error) {
+        console.error(error);
+        alert(error);
+      }
+    }
+
+    loadNotes();
+  }, []);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Yay
-        </p>
-        {/* Bootstrap button! */}
-        <Button onClick={() => setClickCount(clickCount + 1)}>
-          Clicked {clickCount} times
-        </Button>
-      </header>
+      {JSON.stringify(children)}
     </div>
   );
 }
---
+++ frontend/src/models/child.ts
@@ -0,0 +1,8 @@
+export interface Child {
+  _id: string,
+  name: string,
+  gender?: string,
+  age?: number,
+  createdAt: string,
+  updatedAt: string,
+}
